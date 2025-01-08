bd1941f0103c6fed42d136274111e05e81430d03
qingzhixing <qzsptmail@protonmail.com>
2025-01-09 02:03:48 +0800

    🍟 实现选关界面和主菜单双向跳转

--- Scenes/Level Selection Screen/level_selection_screen.gd
+++ Scenes/Level Selection Screen/level_selection_screen.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	MusicController.play_stream(MusicController.music_ChooseYourSeeds);
+
+
+func on_back_to_menu_button_pressed() -> void:
+	SceneSwitcher.jump_to_scene("res://Scenes/Selector Screen/SelectorScreen.tscn")
